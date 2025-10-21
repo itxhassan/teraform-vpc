fd3cfe2462f2ab4e67c3d9295ef6c5b7e60138ed
hassan <m.hassaan2820@gamil.com>
2025-10-21 15:46:07 +0500

    First VPC terraform

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "aws_region" { type = string, default = "us-east-1" }
+variable "vpc_cidr" { type = string, default = "10.0.0.0/16" }
+variable "public_subnets" { type = list(string), default = ["10.0.1.0/24","10.0.2.0/24"] }
+variable "name_prefix" { type = string, default = "demo" }
